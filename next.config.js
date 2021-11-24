2333e8357159654788e21f647f29185944cfa717
jilink <jimmyfac1@gmail.com>
2021-11-24 14:40:36 +0100

    Redirecting to coming-soon page
    We should do the opposite in the future in case people kept the url with
    coming-soon

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   reactStrictMode: true,
+   async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/coming-soon',
+        permanent: true,
+      },
+    ]
+  },
 }
