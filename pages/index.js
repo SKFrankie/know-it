e2010d592c49661bbcb38332c4eae7d7253a08c8
jilink <jimmyfac1@gmail.com>
2021-12-06 11:07:21 +0100

    Know it coming soon page v2 with page turning shape

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
-import { Box, Center, Flex, Text } from '@chakra-ui/layout'
+import { Box, Flex, Spacer, Text } from '@chakra-ui/layout'
+import Image from "next/image"
 import Head from 'next/head'
 
 export default function Home() {
   return (
     <Flex
-      bg="blue.200"
+      bg="#00B9F5"
       color="white"
       minH="100vh"
       p="5"
@@ -18,9 +19,12 @@ export default function Home() {
           content="A learning by gaming Progressive Web App to test and improve your English!"
         />
         <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-<link href="https://fonts.googleapis.com/css2?family=Maven+Pro&display=swap" rel="stylesheet"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Maven+Pro&display=swap"
+          rel="stylesheet"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Ribeye+Marrow&display=swap"
           rel="stylesheet"
@@ -28,38 +32,60 @@ export default function Home() {
       </Head>
 
       <Flex direction="column" as="main" w="80vh" justifyContent="space-around">
+        <Box
+          position="absolute"
+          top="0"
+          left="0"
+          w="150px"
+          h="150px"
+          bg="#007EA7"
+          boxShadow="lg"
+        >
+          <Image
+            src="/../public/images/coming-soon-monster.png"
+            alt="Coming Soon Monster"
+            layout="fill"
+          />
+          <Box className="shape"></Box>
+          <Text
+            fontSize="sm"
+            fontSize={{ base: "xs", md: "sm" }}
+            color="orange"
+            position="relative"
+            w="150px"
+            h="150px"
+            mt="60px"
+            ml="80px"
+            w="120px"
+            transform="rotate(-45deg)"
+          >
+            Either you KNOW IT, or you don't
+          </Text>
+        </Box>
+        <Box height="100px" display={{base: "flex", md: "none"}} />
         <Box>
-          <Text fontFamily="Ribeye Marrow" fontSize={{ base: "80px", md: "100px" }} fontWeight="bold">
+          <Text
+            fontFamily="Ribeye Marrow"
+            fontSize={{ base: "60px", md: "100px" }}
+            fontWeight="bold"
+          >
             KNOW IT!
           </Text>
           <Text pb="5" fontSize={{ base: "md", md: "xl" }}>
             Learning by Gaming
           </Text>
         </Box>
-        <Box
-          fontStyle="italic"
-          fontSize={{ base: "xl", md: "3xl" }}
-          fontWeight="bold"
-        >
+        <Box fontSize={{ base: "xl", md: "3xl" }} fontWeight="bold">
           <Text>Test and improve your English!</Text>
           <Text>Fun, FREE, and Exciting!</Text>
         </Box>
-        <Text
-          transform="rotate(-10deg)"
-          ml="5"
-          w="35vh"
-          p="5"
-          fontSize={{ base: "md", md: "xl" }}
-        >
-          Either you KNOW IT, or you don't
-        </Text>
-        <Text fontSize="3xl" pb="5">
-          Coming soon ...
+        <Text fontSize={{base: "40px", md: "5xl"}} pb="5">
+          Coming soon . . .
         </Text>
       </Flex>
-        <Text position="absolute" right="0" bottom="0" m="5" fontSize="md">
-          a BluePopcorn Production
-        </Text>
+      <Text position="absolute" right="0" bottom="0" m="5" fontSize="md">
+        a BluePopcorn Production
+      </Text>
     </Flex>
   );
 }
