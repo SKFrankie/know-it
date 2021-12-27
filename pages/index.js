cff84aba416606190b8ad7776cb789eca0824cd6
jilink <jimmyfac@gmail.com>
2021-12-27 15:38:15 +0100

    Login button for mobile users

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
-import { Box, Flex, Image} from "@chakra-ui/react";
+import {Icon, Button as ChakraButton, Box, Flex, Image} from "@chakra-ui/react";
 import Head from "next/head";
-// import Podium from "../features/Podium";
-import GaButton from "../features/GaButton";
-import {GAMES_SECTIONS} from "../constants.js";
+import { Icon as Iconify } from '@iconify/react';
+import NextLink from 'next/link'
+
+import { useUserContext } from "../context/user";
 import Title from "../ui/Title.js";
-import {KnowlympicsButton} from "../ui/Button";
+import {GAMES_SECTIONS} from "../constants.js";
+import GaButton from "../features/GaButton";
+import { KnowlympicsButton } from "../ui/Button";
 
 export default function Home() {
+  const [currentUser] = useUserContext();
   return (
     <>
       <Head>
@@ -17,17 +21,48 @@ export default function Home() {
         />
       </Head>
       <Title display={{ base: "none", md: "flex" }} />
+      {currentUser.online ? null : (
+        <NextLink href="/login" passHref>
+          <ChakraButton
+            m={2}
+            boxShadow="0px 2.16px 2.16px rgba(0, 0, 0, 0.25), inset 0px 2.16px 2.16px rgba(0, 0, 0, 0.25)"
+            colorScheme="red"
+            aria-label="login"
+            size="lg"
+            rightIcon={<Icon boxSize={7} color="white" as={Iconify} icon="fe:login" />}
+            position="absolute"
+            right="0"
+            top="0"
+            display={{ base: "flex", md: "none" }}
+          >
+            Login
+          </ChakraButton>
+        </NextLink>
+      )}
       <Flex flexWrap="wrap" w="100%" textAlign="center" justify="center">
         {GAMES_SECTIONS.map((game) => (
-          <Flex direction="column" key={game.name} m="4" width={{ base: "80%", md: "40%" }} textAlign="center" alignItems="center">
-            <Image src={game.image} alt={game.name} w="fit-content" maxH={{base:"40px",md: "80px"}} marginBottom={{base:"-10px", md:"-30px"}}/>
+          <Flex
+            direction="column"
+            key={game.name}
+            m="4"
+            width={{ base: "80%", md: "40%" }}
+            textAlign="center"
+            alignItems="center"
+          >
+            <Image
+              src={game.image}
+              alt={game.name}
+              w="fit-content"
+              maxH={{ base: "40px", md: "80px" }}
+              marginBottom={{ base: "-10px", md: "-30px" }}
+            />
             <GaButton label={game.name} w={{ base: "100%", md: "60%" }} href={game.path}>
               {game.name}
             </GaButton>
           </Flex>
         ))}
       </Flex>
-      <KnowlympicsButton/>
+      <KnowlympicsButton />
     </>
   );
 }
