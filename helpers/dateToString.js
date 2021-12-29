e2dce088a4a6bcc05bb6cb18e90db73ef8ac5574
jilink <jimmyfac@gmail.com>
2021-12-29 15:09:25 +0100

    date helper

---
+++ helpers/dateToString.js
@@ -0,0 +1,11 @@
+const dateToString = (d) => {
+  if (!d) return ''
+
+  const date = new Date(d)
+  const stringDate = `${date.getDate().toString().padStart(2, "0")} ${date
+    .toLocaleString('default', { month: 'long' })
+    .toString()} ${date.getFullYear().toString().padStart(2, "0")}`;
+  return stringDate
+}
+
+export default dateToString
