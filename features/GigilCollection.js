2c43ca8abed20411388931e63d7492da5a818a33
jilink <jimmyfac@gmail.com>
2022-01-03 09:50:09 +0100

    Displaying total avatars

--- features/GigilCollection.js
+++ features/GigilCollection.js
@@ -1,14 +1,26 @@
 import React from 'react'
 import {Text, Flex } from '@chakra-ui/react'
+import { useQuery, useMutation, gql } from "@apollo/client";
+import { basicQueryResultSupport } from "../helpers/apollo-helpers";
 import { useUserContext } from "../context/user";
 import {AvatarImage} from "./shop/AvatarCollections"
 
+const AVATAR_COUNT = gql`
+  query AvatarsCount {
+    avatarsCount
+  }
+`;
+
 const GigilCollection = () => {
   const [currentUser, setCurrentUser] = useUserContext();
+  const { error, data } = useQuery(AVATAR_COUNT, {...basicQueryResultSupport});
   return (
-    <Flex flexWrap="wrap" borderRadius="10" m={2}>
+    <>
+    {data && (<Text mx={5} alignSelf="flex-end" textAlign="right" fontSize="xs">{currentUser?.inventory?.length} / {data.avatarsCount}</Text>)}
+    <Flex flexWrap="wrap" borderRadius="10" placeContent="center" m={2}>
       {currentUser?.inventory?.map((avatar) => <Gigil key={avatar?.avatarId} avatar={avatar}/>)}
     </Flex>
+    </>
   )
 }
 
