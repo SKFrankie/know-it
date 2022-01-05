647b7cb6af35f17759973007440ed0367019369d
jilink <jimmyfac@gmail.com>
2022-01-05 11:42:00 +0100

    Better ux/ui

--- features/games/FabVocabGame.js
+++ features/games/FabVocabGame.js
@@ -71,47 +71,62 @@ const FabVocabGame = () => {
   });
   return (
     <GameContainer game={game} gameState={gameState} setGameState={setGameState}>
-      <Flex
-        direction={{ base: "column", md: "row" }}
-        alignItems={{ base: "center", md: "initial" }}
-        my={2}
-        w="100%"
-        justify="space-around"
-      >
-        <Image
-          maxH={{ base: "50vh", md: "100%" }}
-          w={{ base: "fit-content", md: "100%" }}
-          maxW={{ base: "100%", md: "50vh" }}
-          h={{ base: "100%", md: "fit-content" }}
-          src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+      {data && (
+        <Flex
+          direction={{ base: "column", md: "row" }}
+          alignItems={{ base: "center", md: "initial" }}
           my={2}
-        />
-        {/* GOOGLE AD */}
-        <Flex fontSize="sm"  fontWeight={500} direction="column" alignItems="center">
-          <Text fontWeight={400}>What do you see in the picture ?</Text>
-          <Words words={words} setWords={setWords} tries={wordTries} setTries={setWordTries} />
-          <Divider />
-          <Text my={2} fontWeight={400}>Which sentence best describes this picture?</Text>
-          <Words words={sentences} setWords={setSentences} direction="column" tries={sentenceTries} setTries={setSentenceTries} numbered />
+          w="100%"
+          justify="space-around"
+        >
+          <Image
+            maxH={{ base: "50vh", md: "100%" }}
+            w={{ base: "fit-content", md: "100%" }}
+            maxW={{ base: "100%", md: "50vh" }}
+            h={{ base: "100%", md: "fit-content" }}
+            src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+            my={2}
+          />
+          {/* GOOGLE AD */}
+          <Flex fontSize="sm" fontWeight={500} direction="column" alignItems="center">
+            <Text fontWeight={400}>What do you see in the picture ?</Text>
+            <Text fontSize="xs" fontWeight={400}>
+              {wordTries} words left to find
+            </Text>
+            <Words words={words} setWords={setWords} tries={wordTries} setTries={setWordTries} />
+            <Divider />
+            <Text my={2} fontWeight={400}>
+              Which sentence best describes this picture?
+            </Text>
+            <Words
+              words={sentences}
+              setWords={setSentences}
+              direction="column"
+              tries={sentenceTries}
+              setTries={setSentenceTries}
+              numbered
+            />
+          </Flex>
         </Flex>
-      </Flex>
+      )}
       {error && <Error />}
       {loading && <Loading />}
     </GameContainer>
   );
 };
 
-const Words = ({ words, setWords,tries, setTries, numbered=false, ...props }) => {
+const Words = ({ words, setWords, tries, setTries, numbered = false, ...props }) => {
   // we get as much tries as correct answers
   const handleClick = (word) => {
     console.log("word", tries);
     if (tries > 1) {
       setTries(tries - 1);
-    setWords({ ...words, [word]: { ...words[word], active: true } });
-    if (words[word].correct) {
-      // word correct
-    }
-    return;
+    setWords({ ...words, [word]: { ...words[word], active: true, clicked: true } });
+      if (words[word].correct) {
+        // word correct
+      }
+      // word incorrect
+      return;
     }
     // no more tries
     setTries(0);
@@ -119,16 +134,20 @@ const Words = ({ words, setWords,tries, setTries, numbered=false, ...props }) =>
     Object.keys(tmpWordObject).map(function (key) {
       tmpWordObject[key].active = true;
     });
-    setWords(tmpWordObject);
+    setWords({ ...tmpWordObject, [word]: { ...words[word], active: true, clicked: true } });
   };
 
   const isActive = (word) => {
     return words[word].active;
-  }
+  };
 
   const isCorrect = (word) => {
     return words[word]?.correct;
-  }
+  };
+
+  const isClicked = (word) => {
+    return words[word]?.clicked;
+  };
 
   return (
     <Flex my={3} flexWrap="wrap" justify="center" w="90%" {...props}>
@@ -136,9 +155,11 @@ const Words = ({ words, setWords,tries, setTries, numbered=false, ...props }) =>
         return (
           <Text
             color={isActive(word) ? (isCorrect(word) ? "#07E503" : "#A80909") : "white"}
-            textDecoration={isActive(word) ? (isCorrect(word)? "none" : "line-through") : "none"}
+            textDecoration={
+              !isClicked(word) && isActive(word) ? (isCorrect(word) ? "underline" : "line-through") : "none"
+            }
             cursor={tries > 0 && !isActive(word) ? "pointer" : "default"}
-            onClick={isActive(word) ? null :() => handleClick(word)}
+            onClick={isActive(word) ? null : () => handleClick(word)}
             fontSize="md"
             m={3}
             key={word}
