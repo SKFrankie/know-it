9865c5bb4f4c01b997518a3994af5bf932ccfa93
jilink <jimmyfac@gmail.com>
2022-01-12 11:37:43 +0100

    Reward logic in index page after ranking is done

--- features/modals/RewardPopup.js
+++ features/modals/RewardPopup.js
@@ -1,49 +1,92 @@
-import React from "react";
-import { PopUp } from "../../ui/Modal";
-import {Flex, Text } from "@chakra-ui/react";
+import React, {useState, useEffect} from "react";
 import { useQuery, useMutation, gql } from "@apollo/client";
+import { useUserContext } from "../../context/user";
+import {AvatarImage }from "../shop/AvatarCollections"
+import Modal from "../../ui/Modal";
+import {Flex, Text } from "@chakra-ui/react";
+import {basicQueryResultSupport} from "../../helpers/apollo-helpers";
+import Loading from "../Loading";
+import Error from "../Error";
+import Button from "../../ui/Button";
+
+const COLLECTION_DATA = `
+      avatarCollectionId
+      name
+      avatars {
+        avatarId
+        picture
+      }
+`
 
 const GET_RANKING_REWARDS = gql`
   query GetRankingRewards {
     first: avatarCollections(where: { name_CONTAINS: "MONSTARS1" }) {
-      avatarCollectionId
-      name
-      avatars {
-        picture
-      }
+      ${COLLECTION_DATA}
     }
     second: avatarCollections(where: { name_CONTAINS: "MONSTARS2" }) {
-      avatarCollectionId
-      name
-      avatars {
-        picture
-      }
+      ${COLLECTION_DATA}
     }
     third: avatarCollections(where: { name_CONTAINS: "MONSTARS3" }) {
-      avatarCollectionId
-      name
-      avatars {
+      ${COLLECTION_DATA}
+    }
+  }
+`;
+
+const GET_AVATAR_GIFT = gql`
+  mutation GetAvatarGift($avatarId: ID!) {
+    getAvatarGift(avatarId: $avatarId) {
+      rankingGift
+      inventory {
+        avatarId
         picture
       }
     }
   }
 `;
 
-const RewardPopup = ({ isOpen, onClose, rankingGift=0, collections=[], ...props }) => {
+const RewardPopup = ({ isOpen, onClose, rankingGift = 0, ...props }) => {
+  const [collection, setCollection] = useState([]);
+  const { data, loading, error } = useQuery(GET_RANKING_REWARDS, {
+    onCompleted: (data) => {
+      switch (rankingGift) {
+        case 1:
+          setCollection(data.first[0].avatars);
+          break;
+        case 2:
+          setCollection(data.second[0].avatars);
+          break;
+        case 3:
+          setCollection(data.third[0].avatars);
+
+          break;
+
+        default:
+          break;
+      }
+    },
+    ...basicQueryResultSupport,
+  });
+
   const rewardText = () => {
+      let text;
     switch (rankingGift) {
       case 1:
-        return "Congratulations! You've reached the top of the ranking last time! You get a first place Monstar!";
+        text= "the top";
+        break;
       case 2:
-        return "Congratulations! You've reached second place of the ranking last time! Here's your Monstar!";
+        text= "the second place";
+        break;
       case 3:
-        return "Congratulations! You've reached third place of the ranking last time! Here's your Monstar!";
+        text= "the third place";
+        break;
       default:
         return "No gifts for you this time!";
     }
-  }
+    return `Congratulations, you've reached ${text} of the ranking last time! Select your Monstar!`;
+  };
+
   return (
-    <PopUp isOpen={isOpen} onClose={onClose} {...props}>
+    <Modal isOpen={isOpen} onClose={onClose}  {...props}>
       <Flex
         direction="column"
         alignItems="center"
@@ -55,9 +98,58 @@ const RewardPopup = ({ isOpen, onClose, rankingGift=0, collections=[], ...props 
           You've earned a gift!
         </Text>
         <Text fontSize="sm">{rewardText()}</Text>
+        {loading && <Loading />}
+        {error && <Error />}
+        <Flex flexWrap="wrap" justify="center">
+          {collection.length &&
+            collection.map((avatar) => (
+              <Monstar key={avatar.avatarId} avatar={avatar} onClose={onClose} />
+            ))}
+        </Flex>
       </Flex>
-    </PopUp>
+    </Modal>
   );
-}
+};
 
-export default RewardPopup
+const Monstar = ({ avatar, onClose }) => {
+  const [currentUser, setCurrentUser , {refetch}] = useUserContext();
+  const [canGet, setCanGet] = useState(false);
+  useEffect(() => {
+      const notInInventory =
+        currentUser.inventory.find((i) => i.avatarId === avatar.avatarId) === undefined;
+        setCanGet(notInInventory);
+  }, [currentUser.inventory, avatar.avatarId])
+
+const [getAvatarGift] = useMutation(GET_AVATAR_GIFT, {
+  onCompleted: (data) => {
+    setCanGet(false);
+    setCanGet(false);
+    refetch();
+    onClose();
+  },
+  ...basicQueryResultSupport,
+});
+
+const handleClick = () => {
+  if (canGet) {
+    getAvatarGift({ variables: { avatarId: avatar.avatarId } });
+  }
+};
+
+  return (
+    <Flex
+      direction="column"
+      cursor={canGet ? "pointer" : "initial"}
+      m={3}
+      bg="blueClear.500"
+      p={1}
+      filter={canGet ? "none" : "grayscale(100%);"}
+      onClick={handleClick}
+      _hover={{ bg: "orange" }}
+    >
+      <AvatarImage picture={avatar.picture} />
+    </Flex>
+  );
+};
+
+export default RewardPopup;
