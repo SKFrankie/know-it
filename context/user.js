705787c3cfa9074b36a8aa4da46c5d3a0e09d16a
jilink <jimmyfac@gmail.com>
2022-01-12 12:38:48 +0100

    All ranking gifting logic done

--- context/user.js
+++ context/user.js
@@ -1,6 +1,8 @@
 import { createContext, useContext, useState } from "react";
-import { useQuery, gql } from "@apollo/client";
-const CURRENT_USER = gql`
+import { useQuery, useMutation, gql } from "@apollo/client";
+import { isCurrentWeek } from "../features/games/helpers";
+import { basicQueryResultSupport } from "../helpers/apollo-helpers";
+const CURRENT_USER_AND_TOP_3 = gql`
   query CurrentUser {
     currentUser {
       userId
@@ -14,7 +16,6 @@ const CURRENT_USER = gql`
       createdAt
       points
       lastRankingDate
-      lastRankingGiftDate
       rankingGift
       currentAvatar {
         avatarId
@@ -25,6 +26,19 @@ const CURRENT_USER = gql`
         picture
       }
     }
+    rankingUsers(limit: 3) {
+      userId
+    }
+    lastRankingGiftDate
+  }
+`;
+
+const SET_USERS_GIFTS = gql`
+  mutation SetUsersGifts($first: ID, $second: ID, $third: ID) {
+    setUsersGifts(first: $first, second: $second, third: $third) {
+      userId
+      rankingGift
+    }
   }
 `;
 
@@ -32,10 +46,29 @@ const UserContext = createContext();
 
 export function UserWrapper({ children }) {
   const [currentUser, setCurrentUser] = useState({ online: false, loading: true });
-  const { loading, refetch } = useQuery(CURRENT_USER, {
+  const [SetUsersGifts] = useMutation(SET_USERS_GIFTS, {
+    onCompleted(data) {
+      setCurrentUser({ ...currentUser, ...data.setUsersGifts });
+    },
+    ...basicQueryResultSupport,
+  });
+
+  const { loading, refetch } = useQuery(CURRENT_USER_AND_TOP_3, {
     onCompleted(res) {
       const online = res.currentUser !== null;
       setCurrentUser({ online, loading: false, ...res.currentUser });
+
+      // ranked gift logic
+      // first we get the top 3
+      const [first, second, third] = res.rankingUsers;
+      // then the last time users had a ranked gift
+      const lastRankingGiftDate = new Date(res.lastRankingGiftDate);
+      if(!isCurrentWeek(lastRankingGiftDate)) {
+        // if it was not this week, the first user to log this week triggers the gifting process
+        SetUsersGifts({
+          variables: { first: first?.userId, second: second?.userId, third: third?.userId },
+        });
+      }
     },
     onError(err) {
       setCurrentUser({ online: false, loading: false });
