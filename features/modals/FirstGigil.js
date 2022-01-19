a5d553151366eec9a94ffc836d16a0f484dfa267
jilink <jimmyfac@gmail.com>
2022-01-19 11:39:34 +0100

    Get first gigil monster

--- features/modals/FirstGigil.js
+++ features/modals/FirstGigil.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react'
 import { useUserContext } from "../../context/user";
 import { useQuery, useMutation, gql } from "@apollo/client";
-import { Flex, Image, useDisclosure } from "@chakra-ui/react";
+import { Flex, Text, useDisclosure } from "@chakra-ui/react";
 import Modal from "../../ui/Modal";
 import { AvatarImage } from "../shop/AvatarCollections";
 import { basicQueryResultSupport } from "../../helpers/apollo-helpers";
@@ -22,61 +22,113 @@ const GET_FREE_AVATARS = gql`
   }
 `;
 
+const CHANGE_AVATAR_AND_ADD_TO_INVETORY = gql`
+  mutation ChangeCurrentAvatarAndAddToInventory($avatarId: ID!) {
+    changeCurrentAvatar(avatarId: $avatarId) {
+      avatarId
+      name
+      picture
+    }
+    addAvatarToInventory(avatarId: $avatarId) {
+      avatarId
+    }
+  }
+`;
+
 const FirstGigil = () => {
   const isMonstar = (avatar) => {
     if (!avatar?.collections?.length) return false;
     return avatar.collections.find((collection) => collection.name.includes("MONSTARS"));
   };
 
-  const [currentUser, setCurrentUser] = useUserContext();
+  const [currentUser, setCurrentUser, { refetch }] = useUserContext();
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const [preventReopen, setPreventReopen] = useState(false);
   const [freeAvatars, setFreeAvatars] = useState([]);
   const { loading, error, data } = useQuery(GET_FREE_AVATARS, {
     onCompleted: (data) => {
       setFreeAvatars(data.avatars.filter((avatar) => !isMonstar(avatar)));
     },
+    ...basicQueryResultSupport,
   });
 
+
   useEffect(() => {
-    if (currentUser?.inventory?.length) {
+    if (currentUser?.inventory?.length || !currentUser?.online) {
+      if(isOpen) {
+        onClose();
+      }
       return;
     }
-    if (!isOpen) {
+    if (!isOpen && !preventReopen) {
       onOpen();
-      console.log("currentuser", currentUser?.inventory?.length);
     }
   }, [currentUser]);
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
-      {loading && <Loading />}
-      {error && <Error />}
-      {freeAvatars.length ? (
-        <Flex
-          flexDirection="column"
-          justifyContent="center"
-          alignItems="center"
-          p={4}
-          maxWidth="500px"
-          mx="auto"
-        >
-          {freeAvatars.map((avatar) => (
-            <AvatarImage
-              key={avatar.avatarId}
-              picture={avatar.picture}
-              onClick={() => {
-                onClose();
-                setCurrentUser((prevState) => ({
-                  ...prevState,
-                  inventory: [...prevState.inventory, avatar],
-                }));
-              }}
-            />
-          ))}
-        </Flex>
-      ) : (
-        <></>
-      )}
+      <Flex
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        textAlign="center"
+        p={{ base: 5, md: 10 }}
+      >
+        <Text fontSize="xl" fontWeight="bold">
+          Select your first Gigil Monster!
+        </Text>
+        {loading && <Loading />}
+        {error && <Error />}
+        {freeAvatars.length ? (
+          <Flex flexWrap="wrap" justify="center">
+            {freeAvatars.map((avatar) => (
+              <FirstGigilImage setPreventReopen={setPreventReopen} avatar={avatar} onClose={onClose} key={avatar.avatarId} />
+            ))}
+          </Flex>
+        ) : (
+          <></>
+        )}
+      </Flex>
     </Modal>
+  );
+};
+
+const FirstGigilImage = ({ avatar, onClose, setPreventReopen }) => {
+  const [currentUser, setCurrentUser, { refetch }] = useUserContext();
+  const [changeCurrentAvatarAndAddToInventory, {loading}] = useMutation(CHANGE_AVATAR_AND_ADD_TO_INVETORY, {
+    onCompleted: (data) => {
+      if (refetch) {
+        refetch();
+        setPreventReopen(true);
+        onClose();
+      }
+      setCurrentUser({
+        currentAvatar: data.changeCurrentAvatar,
+        inventory: [data.addToInventory],
+        ...currentUser,
+      });
+    },
+    ...basicQueryResultSupport,
+  });
+
+
+  const handleClick = () => {
+    if (!loading) {
+      changeCurrentAvatarAndAddToInventory({ variables: { avatarId: avatar.avatarId } });
+    }
+  };
+
+  return (
+    <Flex
+      direction="column"
+      cursor="pointer"
+      m={3}
+      bg="blueClear.500"
+      p={1}
+      onClick={handleClick}
+      _hover={{ bg: "orange" }}
+    >
+      <AvatarImage picture={avatar?.picture} />
+    </Flex>
   );
 };
 
