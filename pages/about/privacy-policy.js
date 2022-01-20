a26994be3af1283ff2ee7b74403204821a9d584a
jilink <jimmyfac@gmail.com>
2022-01-20 10:28:03 +0100

    Privacy policy

--- pages/about/privacy-policy.js
+++ pages/about/privacy-policy.js
@@ -1,6 +1,268 @@
-import AboutContainer from '../../features/about/AboutContainer';
+import AboutContainer from "../../features/about/AboutContainer";
+import { Text, UnorderedList, ListItem } from "@chakra-ui/react";
+import Link from "../../ui/Link";
 const PrivacyPolicy = () => {
-  return <AboutContainer>prvaicy policy</AboutContainer>;
+  return (
+    <AboutContainer>
+      <Text m={2} fontSize="xl">
+        Privacy Policy for BluePopcorn Production
+      </Text>
+
+      <Text>
+        At Know It!, accessible from https://www.know-it.bluepopcorn.fun, one of our main priorities
+        is the privacy of our visitors. This Privacy Policy document contains types of information
+        that is collected and recorded by Know It! and how we use it.
+      </Text>
+
+      <Text>
+        If you have additional questions or require more information about our Privacy Policy, do
+        not hesitate to contact us.
+      </Text>
+
+      <Text>
+        This Privacy Policy applies only to our online activities and is valid for visitors to our
+        website with regards to the information that they shared and/or collect in Know It!. This
+        policy is not applicable to any information collected offline or via channels other than
+        this website. Our Privacy Policy was created with the help of the{" "}
+        <Link target="_blank" href="https://www.privacypolicygenerator.info">
+          Free Privacy Policy Generator
+        </Link>
+        .
+      </Text>
+
+      <Text m={2} fontSize="lg">
+        Consent
+      </Text>
+
+      <Text>
+        By using our website, you hereby consent to our Privacy Policy and agree to its terms.
+      </Text>
+
+      <Text m={2} fontSize="lg">
+        Information we collect
+      </Text>
+
+      <Text>
+        The personal information that you are asked to provide, and the reasons why you are asked to
+        provide it, will be made clear to you at the point we ask you to provide your personal
+        information.
+      </Text>
+      <Text>
+        If you contact us directly, we may receive additional information about you such as your
+        name, email address, phone number, the contents of the message and/or attachments you may
+        send us, and any other information you may choose to provide.
+      </Text>
+      <Text>
+        When you register for an Account, we may ask for your contact information, including items
+        such as name, company name, address, email address, and telephone number.
+      </Text>
+
+      <Text m={2} fontSize="lg">
+        How we use your information
+      </Text>
+
+      <Text>We use the information we collect in various ways, including to:</Text>
+
+      <UnorderedList>
+        <ListItem>Provide, operate, and maintain our website</ListItem>
+        <ListItem>Improve, personalize, and expand our website</ListItem>
+        <ListItem>Understand and analyze how you use our website</ListItem>
+        <ListItem>Develop new products, services, features, and functionality</ListItem>
+        <ListItem>
+          Communicate with you, either directly or through one of our partners, including for
+          customer service, to provide you with updates and other information relating to the
+          website, and for marketing and promotional purposes
+        </ListItem>
+        <ListItem>Send you emails</ListItem>
+        <ListItem>Find and prevent fraud</ListItem>
+      </UnorderedList>
+
+      <Text m={2} fontSize="lg">
+        Log Files
+      </Text>
+
+      <Text>
+        Know It! follows a standard procedure of using log files. These files log visitors when they
+        visit websites. All hosting companies do this and a part of hosting services' analytics. The
+        information collected by log files include internet protocol (IP) addresses, browser type,
+        Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the
+        number of clicks. These are not linked to any information that is personally identifiable.
+        The purpose of the information is for analyzing trends, administering the site, tracking
+        users' movement on the website, and gathering demographic information.
+      </Text>
+
+      <Text m={2} fontSize="lg">
+        Cookies and Web Beacons
+      </Text>
+
+      <Text>
+        Like any other website, Know It! uses 'cookies'. These cookies are used to store information
+        including visitors' preferences, and the pages on the website that the visitor accessed or
+        visited. The information is used to optimize the users' experience by customizing our web
+        page content based on visitors' browser type and/or other information.
+      </Text>
+
+      <Text>
+        For more general information on cookies, please read{" "}
+        <Link target="_blank" href="https://www.generateprivacypolicy.com/#cookies">
+          the Cookies article on Generate Privacy Policy website
+        </Link>
+        .
+      </Text>
+
+      <Text m={2} fontSize="lg">
+        Google DoubleClick DART Cookie
+      </Text>
+
+      <Text>
+        Google is one of a third-party vendor on our site. It also uses cookies, known as DART
+        cookies, to serve ads to our site visitors based upon their visit to www.website.com and
+        other sites on the internet. However, visitors may choose to decline the use of DART cookies
+        by visiting the Google ad and content network Privacy Policy at the following URL –{" "}
+        <Link target="_blank" href="https://policies.google.com/technologies/ads">
+          https://policies.google.com/technologies/ads
+        </Link>
+      </Text>
+
+      <Text m={2} fontSize="lg">
+        Our Advertising Partners
+      </Text>
+
+      <Text>
+        Some of advertisers on our site may use cookies and web beacons. Our advertising partners
+        are listed below. Each of our advertising partners has their own Privacy Policy for their
+        policies on user data. For easier access, we hyperlinked to their Privacy Policies below.
+      </Text>
+
+      <UnorderedList>
+        <ListItem>
+          <Text>Google</Text>
+          <Text>
+            <Link target="_blank" href="https://policies.google.com/technologies/ads">
+              https://policies.google.com/technologies/ads
+            </Link>
+          </Text>
+        </ListItem>
+      </UnorderedList>
+
+      <Text m={2} fontSize="lg">
+        Advertising Partners Privacy Policies
+      </Text>
+
+      <Text>
+        You may consult this list to find the Privacy Policy for each of the advertising partners of
+        Know It!.
+      </Text>
+
+      <Text>
+        Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web
+        Beacons that are used in their respective advertisements and links that appear on Know It!,
+        which are sent directly to users' browser. They automatically receive your IP address when
+        this occurs. These technologies are used to measure the effectiveness of their advertising
+        campaigns and/or to personalize the advertising content that you see on websites that you
+        visit.
+      </Text>
+
+      <Text>
+        Note that Know It! has no access to or control over these cookies that are used by
+        third-party advertisers.
+      </Text>
+
+      <Text m={2} fontSize="lg">
+        Third Party Privacy Policies
+      </Text>
+
+      <Text>
+        Know It!'s Privacy Policy does not apply to other advertisers or websites. Thus, we are
+        advising you to consult the respective Privacy Policies of these third-party ad servers for
+        more detailed information. It may include their practices and instructions about how to
+        opt-out of certain options.{" "}
+      </Text>
+
+      <Text>
+        You can choose to disable cookies through your individual browser options. To know more
+        detailed information about cookie management with specific web browsers, it can be found at
+        the browsers' respective websites.
+      </Text>
+
+      <Text m={2} fontSize="lg">
+        CCPA Privacy Rights (Do Not Sell My Personal Information)
+      </Text>
+
+      <Text>Under the CCPA, among other rights, California consumers have the right to:</Text>
+      <Text>
+        Request that a business that collects a consumer's personal data disclose the categories and
+        specific pieces of personal data that a business has collected about consumers.
+      </Text>
+      <Text>
+        Request that a business delete any personal data about the consumer that a business has
+        collected.
+      </Text>
+      <Text>
+        Request that a business that sells a consumer's personal data, not sell the consumer's
+        personal data.
+      </Text>
+      <Text>
+        If you make a request, we have one month to respond to you. If you would like to exercise
+        any of these rights, please contact us.
+      </Text>
+
+      <Text m={2} fontSize="lg">
+        GDPR Data Protection Rights
+      </Text>
+
+      <Text>
+        We would like to make sure you are fully aware of all of your data protection rights. Every
+        user is entitled to the following:
+      </Text>
+      <Text>
+        The right to access – You have the right to request copies of your personal data. We may
+        charge you a small fee for this service.
+      </Text>
+      <Text>
+        The right to rectification – You have the right to request that we correct any information
+        you believe is inaccurate. You also have the right to request that we complete the
+        information you believe is incomplete.
+      </Text>
+      <Text>
+        The right to erasure – You have the right to request that we erase your personal data, under
+        certain conditions.
+      </Text>
+      <Text>
+        The right to restrict processing – You have the right to request that we restrict the
+        processing of your personal data, under certain conditions.
+      </Text>
+      <Text>
+        The right to object to processing – You have the right to object to our processing of your
+        personal data, under certain conditions.
+      </Text>
+      <Text>
+        The right to data portability – You have the right to request that we transfer the data that
+        we have collected to another organization, or directly to you, under certain conditions.
+      </Text>
+      <Text>
+        If you make a request, we have one month to respond to you. If you would like to exercise
+        any of these rights, please contact us.
+      </Text>
+
+      <Text m={2} fontSize="lg">
+        Children's Information
+      </Text>
+
+      <Text>
+        Another part of our priority is adding protection for children while using the internet. We
+        encourage parents and guardians to observe, participate in, and/or monitor and guide their
+        online activity.
+      </Text>
+
+      <Text>
+        Know It! does not knowingly collect any Personal Identifiable Information from children
+        under the age of 13. If you think that your child provided this kind of information on our
+        website, we strongly encourage you to contact us immediately and we will do our best efforts
+        to promptly remove such information from our records.
+      </Text>
+    </AboutContainer>
+  );
 };
 
 export default PrivacyPolicy;
--- pages/about/terms-of-service.js
+++ pages/about/terms-of-service.js
@@ -8,7 +8,7 @@ const TermsOfService = () => {
         <ListItem m={2} fontSize="lg">Terms</ListItem>
 
         <Text>
-          By accessing this Website, accessible from https://www.know-it.bluepopcorn.fun/, you are
+          By accessing this Website, accessible from https://www.know-it.bluepopcorn.fun, you are
           agreeing to be bound by these Website Terms and Conditions of Use and agree that you are
           responsible for the agreement with any applicable local laws. If you disagree with any of
           these terms, you are prohibited from accessing this site. The materials contained in this
