d2f7014e9cecb08ac16ab77697a63b6afc81fbcc
jilink <jimmyfac@gmail.com>
2022-01-20 14:42:52 +0100

    some styling cookie consent

--- features/CookieConsent.js
+++ features/CookieConsent.js
@@ -1,16 +1,16 @@
-import React, {useEffect} from 'react';
-import ReactCookieConsent, {getCookieConsentValue, Cookies} from "react-cookie-consent";
+import React, { useEffect } from "react";
+import ReactCookieConsent, { getCookieConsentValue, Cookies } from "react-cookie-consent";
 import { useUserContext } from "../context/user";
-import { useCookies } from 'react-cookie'
+import { useCookies } from "react-cookie";
 
 const CookieConsent = () => {
-  const [cookies, setCookie] = useCookies(['CookieConsent'])
+  const [cookies, setCookie] = useCookies(["CookieConsent"]);
   const [currentUser] = useUserContext();
   useEffect(() => {
     const isConsent = getCookieConsentValue();
     if (isConsent === "true" || currentUser?.online) {
       // if the user is online he agreed to the privacy policy
-      setCookie('CookieConsent', 'true')
+      setCookie("CookieConsent", "true");
       handleAccept();
     }
     if (isConsent === "false") {
@@ -18,19 +18,24 @@ const CookieConsent = () => {
     }
   }, [currentUser]);
   const handleAccept = () => {
-    console.log("ok alright")
-  }
+    console.log("ok alright");
+  };
   const handleDecline = () => {
-    console.log("not ok this not cool bro, not cool...")
-      //remove google analytics cookies
-      Cookies.remove("_ga");
-      Cookies.remove("_gat");
-      Cookies.remove("_gid");
-  }
+    console.log("not ok this not cool bro, not cool...");
+    //remove google analytics cookies
+    Cookies.remove("_ga");
+    Cookies.remove("_gat");
+    Cookies.remove("_gid");
+  };
 
   return (
     !currentUser.online && (
-      <ReactCookieConsent enableDeclineButton onAccept={handleAccept} onDecline={handleDecline}>
+      <ReactCookieConsent
+        style={{ background: "#00455B" }}
+        enableDeclineButton
+        onAccept={handleAccept}
+        onDecline={handleDecline}
+      >
         This website uses cookies to enhance the user experience.
       </ReactCookieConsent>
     )
