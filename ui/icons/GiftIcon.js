de6481ea51b95d00302fb02ca64c8d813bb9b43a
jilink <jimmyfac@gmail.com>
2022-01-24 14:21:38 +0100

    Gift bounce infinitely if not opened

--- ui/icons/GiftIcon.js
+++ ui/icons/GiftIcon.js
@@ -1,20 +1,65 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Image } from "@chakra-ui/react";
 import MotionBox from "../../features/MotionBox";
+import { useUserContext } from "../../context/user";
+
+const isDateEarlierThanToday = (strDate) => {
+  if (!strDate) {
+    return true;
+  }
+  const date = new Date(strDate);
+  const today = new Date();
+  if (today.getMonth() !== date.getMonth() || today.getYear() !== date.getYear()) {
+    return true;
+  }
+  if (today.getDate() > date.getDate()) return true;
+  return false;
+};
 
 const GiftIcon = ({ ...props }) => {
+  const [currentUser] = useUserContext();
+  const [repeat, setRepeat] = useState(false);
+  useEffect(() => {
+    if (isDateEarlierThanToday(currentUser?.lastSeen)) {
+      setRepeat(true);
+    } else {
+      setRepeat(false);
+    }
+  }, [currentUser]);
+
   return (
-    <MotionBox
-      initial={{ scale: 0.5 }}
-      animate={{ scale: 1 }}
-      transition={{
-        type: "spring",
-        stiffness: 260,
-        damping:5,
-      }}
-    >
-      <Image boxSize="30px" src="/images/gift.png" alt="gift" {...props} />
-    </MotionBox>
+    <>
+      {" "}
+      {repeat && (
+        <MotionBox
+          initial={{ scale: 0.5 }}
+          animate={{ scale: 1 }}
+          transition={{
+            type: "spring",
+            stiffness: 260,
+            damping: 5,
+            repeat: "Infinity",
+            duration: 2,
+          }}
+        >
+          <Image boxSize="30px" src="/images/gift.png" alt="gift" {...props} />
+        </MotionBox>
+      )}
+      {!repeat && (
+        <MotionBox
+          initial={{ scale: 0.5 }}
+          animate={{ scale: 1 }}
+          transition={{
+            type: "spring",
+            stiffness: 260,
+            damping: 5,
+            duration: 2,
+          }}
+        >
+          <Image boxSize="30px" src="/images/gift.png" alt="gift" {...props} />
+        </MotionBox>
+      )}
+    </>
   );
 };
 
