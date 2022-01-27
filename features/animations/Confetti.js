b23c0286377773c9426c2bc2bb3c8f1be1fb5572
jilink <jimmyfac@gmail.com>
2022-01-27 09:43:12 +0100

    more confetti

--- features/animations/Confetti.js
+++ features/animations/Confetti.js
@@ -3,7 +3,7 @@ import ReactConfetti from "react-confetti";
 
 const Confetti = () => {
   const { innerWidth: width, innerHeight: height } = window;
-  return <ReactConfetti width={width} height={height} recycle={false} />;
+  return <ReactConfetti numberOfPieces={600} width={width} height={height} recycle={false} />;
 };
 
 export default Confetti;
