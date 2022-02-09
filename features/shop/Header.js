3b7d87091af32f42127d6820519e72c4a3d2fefb
jilink <jimmyfac@gmail.com>
2022-02-09 10:56:59 +0100

    fontsize and color shop

--- features/shop/Header.js
+++ features/shop/Header.js
@@ -13,7 +13,8 @@ const Header = () => {
         <Link
           fontWeight="500"
           w="50%"
-          color={router.pathname === section.path ? "blueClear.700" : "deepDarkBlue"}
+          fontSize={{ base: "md", md: "2xl" }}
+          color={router.pathname === section.path ? "blueClear.700" : "white"}
           borderBottom={router.pathname === section.path ? "2px solid" : "2px solid transparent"}
           key={section.name}
           href={section.path}
