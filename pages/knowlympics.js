5c7d064fcdcec052a2f2f1b2f281a80739a37c79
jilink <jimmyfac@gmail.com>
2022-02-09 10:35:15 +0100

    i for knowlympics leadeboard page

--- pages/knowlympics.js
+++ pages/knowlympics.js
@@ -2,14 +2,18 @@ import { Flex, Text } from "@chakra-ui/react";
 import { KnowlympicsButton } from "../ui/Button";
 import { useUserContext } from "../context/user";
 import Leaderboard from "../features/Leaderboard";
+import Info from "../features/games/Info";
 
 const Knowlympics = () => {
   const [currentUser] = useUserContext();
   return (
     <Flex direction="column" justify="center" align="center">
-      <Text fontSize={{ base: "3xl", md: "5xl" }} fontWeight="700" color="white" mb={5}>
-        Knowlympics
-      </Text>
+      <Flex>
+        <Text fontSize={{ base: "3xl", md: "5xl" }} fontWeight="700" color="white" mb={5}>
+          Knowlympics
+        </Text>
+        <Info id="knowlympics" />
+      </Flex>
       {/* <Text>What is Knowlympics?</Text> */}
       <KnowlympicsButton text="Start" disabled={!currentUser?.stars} />
       <Leaderboard />
