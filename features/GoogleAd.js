b61fa9155781ec89ad536181f118bfe215c376aa
jilink <jimmyfac@gmail.com>
2022-02-09 13:33:37 +0100

    Add container

---
+++ features/GoogleAd.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { useUserContext } from "../context/user";
+import { isPremium } from "../helpers/premium";
+
+const GoogleAdContainer = ({ children }) => {
+  const [currentUser] = useUserContext();
+  
+  return (!isPremium(currentUser) ? children : <></>);
+
+}
+
+const GoogleAd = ({...props}) => {
+  return <GoogleAdContainer><p>Pub google ici</p></GoogleAdContainer>
+}
+
+
+export default GoogleAd;
