60eecff8988f4efc7e203578a531c1f5e602efc5
jilink <jimmyfac@gmail.com>
2022-02-17 14:44:22 +0100

    better function to get ordinal suffix for rank numbers

--- features/profile/Ranking.js
+++ features/profile/Ranking.js
@@ -31,18 +31,21 @@ const Ranking = ({ userId }) => {
     ...basicQueryResultSupport,
   });
 
-  const getSuperscriptOrdinal = (n) => {
-    switch (n) {
-      case 1:
-        return "st";
-      case 2:
-        return "nd";
-      case 3:
-        return "rd";
-      default:
-        return "th";
+  const getSuperscriptOrdinal = (i) => {
+    let j = i % 10;
+    let k = i % 100;
+    if (j == 1 && k != 11) {
+        return i + "st";
     }
+    if (j == 2 && k != 12) {
+        return i + "nd";
+    }
+    if (j == 3 && k != 13) {
+        return i + "rd";
+    }
+    return i + "th";
   };
+
 
   return (
     <>
@@ -52,7 +55,6 @@ const Ranking = ({ userId }) => {
         <Text fontSize={{ base: "md", md: "3xl" }} p={{ base: "auto", md: "5" }} textAlign="center">
           You are in{" "}
           <Text as="span" color="#F0940B">
-            {rank}
             {getSuperscriptOrdinal(rank)}
           </Text>{" "}
           place!
