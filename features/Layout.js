417ab18304df6d7f54eb1aba6ca9f814606cdd3d
jilink <jimmyfac@gmail.com>
2022-03-13 15:13:50 +0100

    No auto ads for premium users

--- features/Layout.js
+++ features/Layout.js
@@ -3,12 +3,20 @@ import React from "react";
 import { DesktopHeader, MobileNavbar, MobileHeader } from "./Header";
 import CookieConsent from "./CookieConsent";
 import Head from "next/head";
+import { useUserContext } from "../context/user";
+import { isPremium } from "../helpers/premium";
 
 const Layout = ({ children }) => {
+  const [currentUser] = useUserContext();
   return (
     <>
       <Head>
         <title>Know It!</title>
+        {!isPremium(currentUser) && <script
+          data-ad-client={`${process.env.NEXT_PUBLIC_GOOGLE_ADSENSE}`}
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+        />}
       </Head>
       <DesktopHeader />
       <MobileHeader />
